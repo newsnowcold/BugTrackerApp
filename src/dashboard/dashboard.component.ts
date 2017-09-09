e2f574d04cc8fb17dfea0e672baa3c560a6b487d
Darrel Abello <darrel.abello@ilucent.com>
2017-09-09 00:11:30 -0700

    Implement real data for list of issues

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -9,39 +9,89 @@ declare var $:any;
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  title: string;
-  description: string;
-  projects: any;
-  users: any;
+  project: any = {
+    title: undefined,
+    description: undefined
+  };
+  issue: any = {
+    title: undefined,
+    description: undefined
+  };
+  projects: any = new Array();
+  users: any = new Array();
   modal: any;
+  selectedProject: any;
+  issues: any = new Array();
 
   constructor(private http: Http,
               private router: Router) { }
 
   ngOnInit() {
     this.getProjects();
-    this.initializeModals();
-    
+    this.initializeModals();    
   }
-  addProject() {
-    this.http.post('Project/Create', {
-      Name: this.title,
-      Description: this.description
+
+  private addProject() {
+    this.http.post('Project', {
+      Name: this.project.title,
+      Description: this.project.description
     })
     .subscribe(
         result => {
             var data = result.json();
             this.getProjectsHandler(data);
         },
+        err => console.log(err)
+    )
+  }
+
+  private addMembers() {
+    let addMembersUrl = 'Project/' + this.selectedProject.Id + '/members';
+    let tobeNewMembers = new Array();
+
+    for(var i = 0; i < this.users.length; i++) {
+      if (this.users[i].isSelected) {
+        tobeNewMembers.push({
+          "UserId": this.users[i].Id
+        });
+      }
+    }
+
+    this.http.post(addMembersUrl, {
+      Members: tobeNewMembers
+    })
+    .subscribe(
+        result => {
+            console.log(result);
+        },
         err => console.log(err),
         () => this.done()
     )
   }
 
-  done() {
+  private done() {
     $(function () {
-      // $('#modal-addProject').modal('toggle');
+      $('#modal-addProject').modal('toggle');
     });
+  }
+
+  private doneAddingBug() {
+    this.issue.title = undefined;
+    this.issue.description = undefined;
+    $(function () {
+      $('#modal-reportbug').modal('toggle');
+    });
+  }
+
+  private getIssues(projectId) {
+    this.http.get('Issue/' + projectId)
+            .subscribe(
+                result => {
+                  var data = result.json();
+                  this.getIssuesHandler(data);
+                },
+                err => console.log()
+            );
   }
 
   private getProjects() {
@@ -50,6 +100,9 @@ export class DashboardComponent implements OnInit {
                 (result: any) => {
                     var data = result.json();
                     this.projects = data;
+
+                    this.selectedProject = data[0];
+                    this.getIssues(data[0].Id);
                 }
             );
   }
@@ -61,26 +114,65 @@ export class DashboardComponent implements OnInit {
                 var data = result.json();
                 this.getUsersHandler(data);
               },
-              err => console.log(err),
-              () => this.done()
+              err => console.log(err)
             )
+  }
+
+  private save() {
+    if (this.modal.project.IsProjectPhase) {
+      this.addProject();
+    } else {
+      this.addMembers();
+    }
+  }
+
+  private saveBug() {
+    this.http.post('Issue/' + this.selectedProject.Id, {
+      Title: this.issue.title,
+      Description: this.issue.description
+    })
+    .subscribe(
+        result => {
+          this.getIssues(this.selectedProject.Id);
+        },
+        err => console.log(err),
+        () => this.doneAddingBug()
+    )
   }
 
 
   // helper methods
+  private utcToLocalTime(timeString) {
+    if (!timeString) return;
+
+    var time = new Date(timeString.replace('T', ' '))
+    return time.toLocaleDateString();
+  }
+
+  private getIssuesHandler(data: any) {
+    this.issues = [];
+
+    for (var i = 0; i < data.length; i++) {
+      var issue = data[i];
+      issue['index'] = (i + 1);
+      issue.DateCreated = this.utcToLocalTime(issue.DateCreated);
+      issue.DateClosed = this.utcToLocalTime(issue.DateClosed);
+
+      this.issues.push(issue);
+    }
+  }
+
   private getUsersHandler(data: any) {
-    console.log(data)
     for (var i = 0; i < data.length; i++) {
       data[i]['isSelected'] = false;
     }
 
     this.users = data;
-    console.log(this.users)
   }
 
   private getProjectsHandler(data: any) {
-    this.title = undefined;
-    this.description = undefined;
+    this.project.title = undefined;
+    this.project.description = undefined;
     this.modal.project.IsProjectPhase = false;
     this.getUsers();
     // this.getProjects();
